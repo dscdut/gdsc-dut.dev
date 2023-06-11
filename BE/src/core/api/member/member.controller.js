5f91ebe2b9844f1dad3c7e6a35176e8019a127ba
NguyenThang.2302 <119683462+nguyenThang2302@users.noreply.github.com>
2023-06-11 20:38:39 +0700

    WG-97: Add IdGen Phone Email For Get Members

--- BE/src/core/api/member/member.controller.js
+++ BE/src/core/api/member/member.controller.js
@@ -32,7 +32,49 @@ class Controller {
     findAll = async req => {
         const reqTransformed = new RequestTransformer(req.query, searchMemberSchema);
         const data = await this.service.getAndCount(reqTransformed);
-        return ValidHttpResponse.toOkResponse(data);
+        const groupByResults = data.content.reduce((acc, current) => {
+            const existingItem = acc.find(item => item.id === current.id);
+            if (existingItem) {
+                existingItem.gen.push({
+                    id: current.gen_id,
+                    name: current.gen_name,
+                });
+                existingItem.department.push({
+                    id: current.department_id,
+                    name: current.department_name
+                });
+                existingItem.position.push({
+                    id: current.position_id,
+                    name: current.position_name
+                });
+            } else {
+                acc.push({
+                    id: current.id,
+                    full_name: current.full_name,
+                    birthday: current.birthday,
+                    phone: current.phone,
+                    email: current.email,
+                    horoscope_sign: current.horoscope_sign,
+                    philosophy: current.philosophy,
+                    feelings: current.feelings,
+                    infor_url: current.infor_url,
+                    avatar_url: current.avatar_url,
+                    gen: { id: current.gen_id, name: current.gen_name },
+                    department: { id: current.department_id, name: current.department_name },
+                    position: { id: current.position_id, name: current.position_name },
+                    createdAt: current.createdAt,
+                    updateAt: current.updateAt,
+                    deleteAt: current.deleteAt
+                });
+            }
+            return acc;
+        }, []);
+
+        const finalResult = Object.values(groupByResults).map(obj => {
+            const { gen_id, gen_name, ...rest } = obj;
+            return rest;
+        });
+        return ValidHttpResponse.toOkResponse(finalResult);
     };
 }
 
